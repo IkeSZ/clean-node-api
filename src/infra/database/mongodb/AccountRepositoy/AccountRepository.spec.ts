dc8fec926cbc796dd50d72da46e50c88c67584f5
IkeSZ <isaacsouzafernandes1@hotmail.com>
2021-03-24 18:50:01 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/database/mongodb/AccountRepositoy/AccountRepository.spec.ts
@@ -0,0 +1,37 @@
+import { AccountMongoRepository } from './AccountRepository'
+
+// Helpers
+import { MongoHelper } from '../helpers/MongoHelper'
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
+describe('AccountRepository MongoDb', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('should be able to return an account on success', async () => {
+    const sut = makeSut()
+
+    const accountData = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: '123456'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('123456')
+  })
+})
