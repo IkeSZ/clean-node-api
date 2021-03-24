60c38210a29e94c509cf06d3c7be8051bd38e9dc
IkeSZ <isaacsouzafernandes1@hotmail.com>
2021-03-24 16:25:32 -0300

    feat: ensure DbAddAccount returns a account on success

--- src/data/usecases/AddAccount/DbAddAccount.ts
+++ src/data/usecases/AddAccount/DbAddAccount.ts
@@ -14,9 +14,9 @@ class DbAddAccount implements IAddAccount {
 
   public async add (accountData: IAddAccountModel): Promise<IAccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
 
-    return new Promise(resolve => resolve(null))
+    return new Promise(resolve => resolve(account))
   }
 }
 
